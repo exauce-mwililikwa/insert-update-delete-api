472e9548881823db1ccfc09e7bbf45e49aac8c1f
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-06-01 01:00:08 +0200

    migration

--- flutter_food_app/api/foods/getFoods.php
+++ flutter_food_app/api/foods/getFoods.php
@@ -6,7 +6,7 @@ $result=array();
 $queryFood="select * from foods";
 $queryResult=mysqli_query($conn,$queryFood);
 while ($row=mysqli_fetch_assoc($queryResult)) {
-//$result=$row;s]sncsnsnnxnsxncbxbsmcsnssssnscncnsscnsssssxnxnxmsssssssnssssssssssssssssxnssssnnsxssnsnsnsxsnsnsnnssnsssnssnsnnsxnsxsss
+//$result=$row;s]sncsnsnnxnsxncbxxbsmcsnssssnscncnsscnsssssxnxnxmsssssssnssssssssssssssssxnssssnnsxssnsnsnsxsnsnsnnssnsssnssnsnnsxnsxsss
 array_push($result,$row);
 }s
 echo json_encode($result);
